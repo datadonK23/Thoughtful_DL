1cb262ab00f56d3a0693b8658fe035e1fdd2e3ce
datadonK23 <datadonk23@gmail.com>
2018-06-13 14:17:09 +0200

    Preprocess reviews

--- IMDB_GloVE_classification/preprocessing.py
+++ IMDB_GloVE_classification/preprocessing.py
@@ -8,15 +8,18 @@ Author: datadonk23 (datadonk23@gmail.com)
 Date: 2018-06-12
 """
 import os
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+import numpy as np
 
 
 def preprocess_labels(data_dir_path="data/aclImdb", dataset="train"):
     """
     Parses aclImdb data directory and generates two lists. One contains text of the review and other the corresponding
     label, which is 0 for negative and 1 for positive ratings.
-    :data_dir_path: String, path to dataset directory
-    :dataset: String, either "train" for training or "test" for testing dataset
-    :return: texts=[review_text], labels=[0|1] - label: 0 = "neg", 1 = "pos" rating
+    :param data_dir_path: String, path to dataset directory
+    :param dataset: String, either "train" for training or "test" for testing dataset
+    :return: texts=[review_text], labels=np.ndarray([0|1]) - label: 0 = "neg", 1 = "pos" rating
     """
     if dataset not in ["train", "test"]:
         raise ValueError('Specify correct dataset: either "train" or "test"')
@@ -35,6 +38,30 @@ def preprocess_labels(data_dir_path="data/aclImdb", dataset="train"):
                     labels.append(0)
                 else:
                     labels.append(1)
+    labels = np.asarray(labels)
 
     return texts, labels
 
+
+def tokenize_data(texts, max_len=100, max_words=1000):
+    """
+    Vectorize reviews
+    :param texts: list of review texts
+    :param max_len: max length of review text
+    :param max_words: max number of words to consider in dataset
+    :return: np.ndarray of shape (len(sequences), max_len)
+    """
+    tokenizer = Tokenizer(num_words=max_words)
+    tokenizer.fit_on_texts(texts)
+    sequences = tokenizer.texts_to_sequences(texts)
+    padded_sequences = pad_sequences(sequences, maxlen=max_len)
+
+    return padded_sequences
+
+
+def split_data():
+    """
+    Split data into train and validation splits
+    :return:
+    """
+    pass
--- IMDB_GloVE_classification/test/test_preprocessing.py
+++ IMDB_GloVE_classification/test/test_preprocessing.py
@@ -1,13 +1,14 @@
 import unittest
-import os
+import numpy as np
 
-from preprocessing import preprocess_labels
+from preprocessing import preprocess_labels, tokenize_data
 
 
 class ModelUtilsTests(unittest.TestCase):
 
     def setUp(self):
         self.mock_data_path = "data/mock_aclImdb"
+        self.mock_texts = ["First mocked review.", "Second mocked review."]
 
 
     def tearDown(self):
@@ -21,14 +22,34 @@ class ModelUtilsTests(unittest.TestCase):
         texts_train, labels_train = preprocess_labels(self.mock_data_path, "train")
         texts_test, labels_test = preprocess_labels(self.mock_data_path, "train")
 
-        self.assertTrue(texts_train, "Train texts not generated")
-        self.assertTrue(labels_train, "Train labels not generated")
-        self.assertTrue(texts_test, "Test texts not generated")
-        self.assertTrue(labels_test, "Test labels not generated")
+        self.assertEqual(set([0, 1]), set(labels_train), "Labels list contains not only 0 or 1 in trainset")
+        self.assertEqual(set([0, 1]), set(labels_test), "Labels list contains not only 0 or 1 in testset")
 
-        self.assertEqual(set(labels_train), set([0, 1]), "Labels list contains not only 0 or 1 in trainset")
-        self.assertEqual(set(labels_test), set([0, 1]), "Labels list contains not only 0 or 1 in testset")
+        self.assertEqual(len(labels_train), len(texts_train), "Train texts and labels lists have different length")
+        self.assertEqual(len(labels_test), len(texts_test), "Test texts and labels lists have different length")
 
-        self.assertEqual(len(texts_train), len(labels_train), "Train texts and labels lists have different length")
-        self.assertEqual(len(texts_test), len(labels_test), "Test texts and labels lists have different length")
+        self.assertEqual(np.ndarray, type(labels_train), "Train labels not a numpy array")
 
+
+    def test_tokenize_data(self):
+        """
+        Test tokenization of review data
+        """
+        token100_vector = tokenize_data(self.mock_texts)
+
+        self.assertEqual(np.ndarray, type(token100_vector), "Tokenized data not a numpy array")
+        self.assertEqual(2, len(token100_vector), "Incorrect number of returned sequences")
+
+        token20_vector = tokenize_data(self.mock_texts, 20)
+        self.assertEqual(20, len(token20_vector[0]), "Incorrect length of token vector")
+        self.assertEqual(len(token20_vector[0]), len(token20_vector[1]), "Token vectors don't match on length")
+
+        token20__wordlimit_vector = tokenize_data(self.mock_texts, 20, 4)
+        self.assertEqual(3, token20__wordlimit_vector.max(), "Incorrect number of words in word-limited vector")
+
+
+    def test_split_data(self):
+        """
+        Test train and validation split
+        """
+        pass
